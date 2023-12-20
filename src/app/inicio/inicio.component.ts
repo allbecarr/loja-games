5eed2f7cbb96fbf555318d5732c287d3d038596d
allbecarr <allbecarr@gmail.com>
2023-12-20 12:30:22 -0300

    Desenvolvimento do Componente Card

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+//Imports Angular Materials
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+//-----------------------------
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
